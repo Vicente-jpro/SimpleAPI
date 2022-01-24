2fddbea50ecf49694939f611dc4ba30283ef4a9d
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-01-24 23:52:36 +0100

    API uptated

--- src/main/java/com/example/demo/controller/EstudentController.java
+++ src/main/java/com/example/demo/controller/EstudentController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,8 +34,9 @@ public class EstudentController {
 	}
 	
 	@PostMapping("add")
-	public @ResponseBody String create(@RequestParam String name, @RequestParam String phoneNumber ){
-		studentServicedb.create( new Student(name, phoneNumber) );
+	@ResponseBody
+	public String create(@RequestBody Student student ){
+		studentServicedb.create( student ) ;
 		return "Student saved";
 	}
 	
@@ -43,18 +45,17 @@ public class EstudentController {
 		return studentServicedb.readAll();
 	}
 	
+	// localhost:8000/student/1?id=idNumber
 	@GetMapping("{id}")
-	public @ResponseBody Student getStudent(@PathVariable Long id){
+	public @ResponseBody Student getStudent(@RequestParam Long id){
 		return this.studentServicedb.getStudent(id);
 	}
 	
 	@DeleteMapping("{id}")
-	public @ResponseBody String Delete(@PathVariable Long id){
+	public @ResponseBody String Delete(@RequestParam Long id){
 		this.studentServicedb.delete(id);
 		return "Student deleted";
 	}
-	@PatchMapping("update")
-	public @ResponseBody String update(@RequestParam Long id, @RequestParam String name, @RequestParam String phoneNumber){
-		return "";
-	}
+	
+	
 }
