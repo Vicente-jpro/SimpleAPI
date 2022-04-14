7f360bd58d16ef88785863d98c56b19868d67e0d
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-04-14 01:56:50 +0100

    Unit test

---
+++ src/main/java/com/example/demo/TextService.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TextService {
+	
+	public String hello() {
+		return "Hello Spring test";
+	}
+}
