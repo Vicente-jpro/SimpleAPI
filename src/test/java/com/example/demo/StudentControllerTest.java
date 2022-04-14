44553754985f0e2f3c93be7260c48b92be7d5b01
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-04-14 19:47:29 +0100

    Fixing bugger on getStudent test controller

--- src/test/java/com/example/demo/StudentControllerTest.java
+++ src/test/java/com/example/demo/StudentControllerTest.java
@@ -9,50 +9,46 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.demo.domain.Student;
 import com.example.demo.execeptions.StudentNotFoundExeception;
 import com.example.demo.service.StudentServicedb;
 
-@SpringBootTest
-class StudentServiceTest {
-
-	@Autowired
-	private TextService textService;
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class StudentControllerTest {
 	
 	@Autowired
 	private StudentServicedb studentServicedb;
 	
-	private Student student;
+	@Autowired
+	private TestRestTemplate testRestTemplate;
 	
-	@Test
-	public void itShoudDisplayAgreatMessage() {
-		textService = new TextService();
-		String result = textService.hello();
-		Assertions.assertThat( result.equalsIgnoreCase("Hello Spring test") );
-	}
+	private Student student;
 	
 	
 	@Test
 	@DisplayName("Get all students when there is")
 	@Sql(statements = "INSERT INTO student (name, phone_number) values ('Vicente', '944889722')")
 	public void readAll() {
-	
-		List<Student> students = studentServicedb.readAll();
-		assertEquals( students.isEmpty(), false );
-		
+		ResponseEntity<Student> students = testRestTemplate.getForEntity("/students", Student.class);
+		assertEquals( students.hasBody() , true );
 	}
 	
 	
 	@Test
 	@DisplayName("Get student")
 	public void getStudent() {
-		student = studentServicedb.findLastStudent();
-		
-		Student st = studentServicedb.getStudent( (long) student.getId() );
-		assertEquals( st.getName(), "Vicente" );
+		student = studentServicedb.findLastStudent();	
+		System.err.println(student.toString());
+		ResponseEntity<Student> st = testRestTemplate.getForEntity("/get/"+student.getId(), Student.class);
+		assertEquals( st.getBody().getName() , "Vicente" );
 	}
 	
 	@Test
